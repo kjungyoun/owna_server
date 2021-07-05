cd3e1e98629ea5b6a99400fdf95cfa752b439769
kjungyoun <rlawjddbs963@naver.com>
2021-07-05 21:48:02 +0900

    git init

---
+++ src/main/java/com/owna/OwnaBackApplication.java
@@ -0,0 +1,13 @@
+package com.owna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owna/OwnaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
